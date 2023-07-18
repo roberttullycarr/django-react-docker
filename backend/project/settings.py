2f91e4c576716412fecc1bebbbdeffda1b22e8c0
Robert Carr <roberttullycarr@gmail.com>
2023-07-18 18:09:37 +0200

    added correct declaration of static files

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -138,8 +138,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static-files/'
+STATIC_ROOT = '/static-files/'
+
+MEDIA_URL = '/media-files/'
+MEDIA_ROOT = '/media-files/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
