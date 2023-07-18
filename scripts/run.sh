6eebe1f2e663ae7f16b49ed961a4dd6c2f5edfe7
Robert Carr <roberttullycarr@gmail.com>
2023-07-18 12:46:58 +0200

    changed values in run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 # remove content of front (previous data) and copy from tmp folder to /frontend to reflect updates
 python manage.py migrate
 python manage.py collectstatic --no-input
-gunicorn -w 4 -b 0.0.0.0:8000 project_settings.wsgi:application
+gunicorn -w 4 -b 0.0.0.0:8000 project_settings.wsgi.application
 
 
 
